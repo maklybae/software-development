0738a6240f3f36f3274194c8b78934b342395e96
Maksim Klychkov <maxim.klychkov64@gmail.com>
2025-03-19 00:46:42 +0300

    feat: :sparkles: accounts facade and new methods in entities

--- HSEBank/HSEBank.Entities/Core/BankAccount.cs
+++ HSEBank/HSEBank.Entities/Core/BankAccount.cs
@@ -2,16 +2,55 @@ namespace HSEBank.Entities.Core;
 
 public class BankAccount : IIdentifiable
 {
+    private string _name;
+    private decimal _balance;
+    
     public Guid Id { get; private set; }
-    
-    public string Name { get; private set; }
-    
-    public decimal Balance { get; private set; }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name cannot be null or empty.");
+            }
+
+            _name = value;
+        }
+    }
+
+    public decimal Balance
+    {
+        get => _balance;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Balance cannot be negative.");
+            }
+            
+            _balance = value;
+        }
+    }
     
     public BankAccount(string name, decimal balance)
     {
         Name = name;
         Balance = balance;
         Id = Guid.NewGuid();
+    }
+    
+    public void IncreaseBalance(decimal amount)
+    {
+        // Validation in the setter
+        Balance += amount;
+    }
+
+    public void DecreaseBalance(decimal amount)
+    {
+        // Validation in the setter
+        Balance -= amount;
     }
 }
---
+++ HSEBank/HSEBank.UseCases/Facades/BankAccountFacade.cs
@@ -0,0 +1,73 @@
+using HSEBank.Entities.Core;
+using HSEBank.UseCases.Factories;
+using HSEBank.UseCases.Repository;
+
+namespace HSEBank.UseCases.Facades;
+
+public class BankAccountFacade
+{
+    private readonly IAccountsRepository _accountsRepository;
+    private readonly ICoreEntitiesCreator _factory; 
+
+    public BankAccountFacade(IAccountsRepository accountsRepository, ICoreEntitiesCreator factory)
+    {
+        _accountsRepository = accountsRepository ?? throw new ArgumentNullException(nameof(accountsRepository));
+        _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+    }
+    
+    public BankAccount CreatePostAccount(string name, decimal balance)
+    {
+        var account = _factory.CreateBankAccount(name, balance);
+        _accountsRepository.Add(account);
+        return account;
+    }
+
+    public BankAccount GetById(Guid id)
+    {
+        return _accountsRepository.GetById(id);
+    }
+    
+    public BankAccount GetUniqueByName(string name)
+    {
+        var account = _accountsRepository.GetByName(name).FirstOrDefault();
+        return account ?? throw new InvalidOperationException($"Account with name {name} was not found");
+    }
+
+    public IEnumerable<BankAccount> GetAll()
+    {
+        return _accountsRepository.GetAll();
+    }
+    
+    public IEnumerable<BankAccount> GetAccounts()
+    {
+        return _accountsRepository.GetAll();
+    }
+    
+    public void UpdateNameById(Guid id, string name)
+    {
+        var account = _accountsRepository.GetById(id);
+        account.Name = name;
+        _accountsRepository.Update(account);
+    }
+    
+    public void IncreaseBalanceById(Guid id, decimal amount)
+    {
+        var account = _accountsRepository.GetById(id) ?? 
+                      throw new InvalidOperationException($"Account with name {id} was not found");
+        account.IncreaseBalance(amount);
+        _accountsRepository.Update(account);
+    }
+
+    public void DecreaseBalanceById(Guid id, decimal amount)
+    {
+        var account = _accountsRepository.GetById(id) ?? 
+                      throw new InvalidOperationException($"Account with name {id} was not found");
+        account.DecreaseBalance(amount);
+        _accountsRepository.Update(account);
+    }
+    
+    public void DeleteAccount(Guid id)
+    {
+        _accountsRepository.Delete(id);
+    }
+}
